1d22fbdc690c337b71a99747cb3644aed672ebc7
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-06-27 13:04:35 -0300

    Adding the user input for the array.

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ import { calculaMedia } from "./calculaMedia.js";
 import { multiplicaArray } from "./multiplicaArray.js";
 import { somaArray } from "./somaArray.js";
 
-// let User = prompt("Insert the array.")
-// let arrayUser = (User.split(",")).map(x => Number(x))
+let User = prompt("Insert the array. (Separate the numbers by \",\")")
+let arrayUser = (User.split(",")).map(x => Number(x.trim()))
 
-let arrayUser = ("5,2,13,4,9".split(",")).map(x => Number(x))
+// let arrayUser = ("5,2,13,4,9".split(",")).map(x => Number(x))
 
-alert(arrayUser)
+alert(`The typed array is [${arrayUser}]`)
 
 somaArray(arrayUser)
 calculaMedia(arrayUser)
