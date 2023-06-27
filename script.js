6db615e4bfcacccb96109fdefd8057c6ec9f58a4
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-06-27 15:26:06 -0300

    Creating the object containing the students and their information.

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let index = 0
+
+let alunos = [{
+    nome: "Alvaro Pimentel",
+    nota1: 5.7,
+    nota2: 7.3
+}, {
+    nome: "Maria Nilda",
+    nota1: 9.7,
+    nota2: 8.4
+}, {
+    nome: "João Victório",
+    nota1: 6.8,
+    nota2: 9.3
+}, {
+    nome: "Patrícia Rocha",
+    nota1: 8.9,
+    nota2: 7.1
+}
+]
+
+console.log(alunos)
+console.log(`\n\nPara o(a) aluno(a) de índice ${index}\n\nNome: ` + alunos[index].nome, "\nNota 1: " + alunos[index].nota1, "\nNota 2: " + alunos[index].nota2)
