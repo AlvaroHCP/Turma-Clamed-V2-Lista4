7106216d26d27a929d86a8131e7a7b6a2fef8314
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-07-24 22:17:31 -0300

    Importando as funções na página principal.

---
+++ script.js
@@ -0,0 +1,7 @@
+import somaImpares from "./somaImpares.js"
+import filtrarPares from "./filtrarPares.js"
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+console.log(filtrarPares(array) + "\n")
+console.log(somaImpares(array))
