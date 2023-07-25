782f039c70626a7aa93739a43eb194ac9d87a596
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-07-24 22:11:00 -0300

    FIX: Consertando erro na função somaImpares.

--- somaImpares.js
+++ somaImpares.js
@@ -2,6 +2,8 @@ function somaImpares(array) {
     let somaImpares = array.reduce((total, n) => {
         if (n % 2 != 0) {
             return total += n
+        } else {
+            return total
         }
     }, 0)
 
